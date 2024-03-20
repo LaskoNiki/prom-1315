73d8a88317babe142bde39d56f43361db93703e5
lasko <laskova.nikola.nl@gmail.com>
2024-03-20 13:44:39 +0100

    rozšíření třídy hlavní

--- src/main/java/Hlavni.java
+++ src/main/java/Hlavni.java
@@ -1,7 +1,10 @@
+/*
+ * projekt*/
 public class Hlavni
 {
     public static void main(String[] args)
     {
         System.out.println("Hlavni třída");
+        System.out.println("Přidáno v klonu");
     }
 }
